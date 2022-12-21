7659829489bee00d46fafdfc7aadba5854e2160d
BregozZ <81527182+BregozZ0@users.noreply.github.com>
2022-12-21 18:10:08 +0300

    Update YoutubeVideoDonusturucu.py

--- YoutubeVideoDonusturucu.py
+++ YoutubeVideoDonusturucu.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 
-link = input("Link : ")
-
+link = input("Enter the link: ")
+yt = YouTube(link)
+ys = yt.streams.get_highest_resolution()
+ys.download()
 print("İndiriliyor...")
-YouTube(link).streams.first().download()
-print("İndirildi :)")
